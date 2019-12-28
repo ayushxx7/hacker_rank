804d78b8ea4b95e7ef7ba75ce3bcb86da7e7a8ae
ayushxx7 <ayushxx7@gmail.com>
2019-12-28 22:54:03 +0530

    jumping clouds problem

---
+++ counting_clouds.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+import os
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+
+    index = 0
+    step = -1
+    size = len(c)
+
+    while index < size:
+        print(c[index])
+        if index + 2 < size and c[index] == 0:
+            index = index + 1
+        step += 1
+        index += 1
+    
+    return step
+    # step_count = -1
+    # print(c)
+    # for index,step in enumerate(c):
+    #     print(c[index])
+    #     if index+2 < len(c):
+    #         if c[index+2] == 0:
+    #             index = index+1
+    #             step_count += 1
+    #     else:
+    #         step_count += 1
+    # return step_count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
